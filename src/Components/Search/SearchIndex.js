8d40a0dbfcaa942400a489dbbfcafa94d53a892b
Tony Thomas <tonythomas504@gmail.com>
2020-12-13 22:36:29 -0500

    pushing react legacy commit. Code is completed and working

--- src/Components/Search/SearchIndex.js
+++ src/Components/Search/SearchIndex.js
@@ -1,21 +1,43 @@
 import React from 'react';
 import {Input} from 'reactstrap';
+import { render } from 'react-dom';
  
-const SearchIndex extends Component() {
+class SearchIndex extends React.Component {
+  constructor(props) {
+    super(props)
    this.state = {
-     things: ['pen', 'marker', 'eraser', 'notebook', 'pencil', 'scissors', 'highlighter', 'stapler', 'paper clip', 'binder', 'hole punch', 'laminator', 'laminating sheets', 'protective sheets', 'index cards']
+     things: ['pen', 'marker', 'eraser', 'notebook', 'pencil', 'scissors', 'highlighter', 'stapler', 'paper clip', 'binder', 'hole punch', 'laminator', 'laminating sheets', 'protective sheets', 'index cards'], searchTerm: ''
    }
+  }
+  searchFunction(e) {
+   this.setState({
+     searchTerm: (e)
+   })
  }
 
- function searchFunction() {
- }
 
  render() {
+   return (
      <div>
-       <Input placeholder='Search Here' />
-       <h3>Results:</h3>
+      <Input type='text' placeholder='Search Here' onChange={(e) => this.searchFunction(e.target.value)} />
+      <h3>Results:</h3>
+      {console.log(this.state.things)}
+      {this.state.things 
+       .filter(thing => {
+         if (thing.includes(this.state.searchTerm)) {
+           return true
+         }
+       })
+       .map((thing,index) => {
+         return (
+           <div key={index}>
+           <h3>{thing}</h3>
+           </div>
+         )
+       })
+      }
      </div>
+   )
  }
-
- 
-export SearchIndex;
+}
+export default SearchIndex
